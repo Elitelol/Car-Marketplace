aca603bbcf24c41f04ce31d55ed789f240ac90b7
Elitelol <58183375+Elitelol@users.noreply.github.com>
2021-04-20 14:02:45 +0300

    Connection to MongoDB added
    installed cors package

--- back/index.js
+++ back/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+const DB_CONNECTION = 'mongodb+srv://cargoesvroom:cargoesvroom123@cluster0.kryum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'; 
+const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.send("CAR GOES VROOOOOM");
 });
 
-app.listen(5000, () => { console.log("Server running") });
+mongoose.connect(DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+    .catch((error) => console.log(error.message));
