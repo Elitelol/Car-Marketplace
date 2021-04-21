6ae647835df3376520c4584b9c225865c16348ed
Elitelol <58183375+Elitelol@users.noreply.github.com>
2021-04-21 12:55:58 +0300

    Added getting every user's cars

--- back/controllers/cars.js
+++ back/controllers/cars.js
@@ -26,7 +26,7 @@ const getUserCars = async(req, res) => {
     try{
         const user = await User.find({username}).populate("cars");
         
-        res.status(201).json(user[0]["cars"]);
+        res.status(200).json(user[0]["cars"]);
     }
     catch(err){
         res.status(500).json({message: "Something wrong with server"});
@@ -35,6 +35,48 @@ const getUserCars = async(req, res) => {
 
 const getCar = async (req, res) => {
     const {username, carId} = req.params;
+
+    try{
+        const user = await User.find({username}).populate("cars");
+        const cars = user[0]["cars"];
+
+        for(i = 0; i < cars.length; i++){
+            const car = cars[i];
+
+            if(car._id == carId){
+               return res.status(200).json(car);       
+            }
+        }
+
+    }
+    catch(err){
+        res.status(500).json({message: "Something wrong with server"});
+    }
 }
 
-module.exports = {addCar, getUserCars, getCar};
+const getAllCars = async (req, res) => {
+    try{
+        const allUsers = await User.find().populate("cars");
+        const cars = [];
+
+        for(i = 0; i < allUsers.length; i++){
+            const currentCars = allUsers[i]["cars"];
+
+            for(j = 0; j < currentCars.length; j++){
+                cars.push(currentCars[j]);
+            }
+        }
+        
+        res.status(200).send(cars);   
+    }
+    catch(err){
+        res.status(500).json({message: "Something wrong with server" + err});
+    }
+}
+
+module.exports = {
+     addCar,
+     getUserCars, 
+     getCar,
+     getAllCars
+};
--- back/routes/cars.js
+++ back/routes/cars.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const auth = require('../middleware/auth');
-const {addCar, getUserCars, getCar} = require('../controllers/cars');
+const {addCar, getUserCars, getCar, getAllCars} = require('../controllers/cars');
 
 const router = express.Router();
 
 router.post("/:username/create", addCar);
 router.get("/:username", getUserCars);
-router.get("/:username/:carId", getCar)
+router.get("/:username/:carId", getCar);
+router.get("", getAllCars);
 
 module.exports = router
