bf70110b972280d589bfff0f3959f3d5b23de8ab
Elitelol <58183375+Elitelol@users.noreply.github.com>
2021-04-22 22:07:55 +0300

    Added User update, get user (prob not working)
    fuck docker

--- back/routes/users.routes.js
+++ back/routes/users.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { signIn, signUp, getUser, deleteUser } = require('../controllers/users.controller');
+const { signIn, signUp, getUser, deleteUser, getUsers, updateUser} = require('../controllers/users.controller');
 const auth = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/:username', getUser);
+router.get("/all", getUsers);
 router.delete('/delete/:username', auth, deleteUser);
+router.patch("/update/:username", auth, updateUser);
 router.post('/signIn', signIn);
 router.post('/signUp', signUp);
 
