c31995137967b5329c343e9faa4eae446bc11603
Jaroslav <jarrzo@outlook.com>
2021-04-23 02:11:55 +0300

    Move login api to UsersRepository

--- front/src/services/api/users.jsx
+++ front/src/services/api/users.jsx
@@ -1,3 +1,30 @@
-class UsersRepository {}
+import axios from "axios";
+import { toast } from "react-toastify";
+import API_CONFIG from "../../config/api.config";
+import authHeader from "../authHeader";
+import authService from "../auth.service";
+
+class UsersRepository {
+  login(comp) {
+    axios
+      .post(API_CONFIG.URL + "/users/signIn", {
+        username: comp.state.username,
+        password: comp.state.password,
+      })
+      .then((res) => {
+        authService.setToken(res.data);
+        toast.success("Success!");
+        window.location.reload();
+      })
+      .catch((error) => {
+        if (error != null && error.response != null)
+          toast.error(error.response.data.message);
+        else
+          toast.error(
+            "Something wrong happend!\nPlease contact technical support."
+          );
+      });
+  }
+}
 
 export default new UsersRepository();
