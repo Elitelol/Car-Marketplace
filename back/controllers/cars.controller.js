22edbb5dceb2d1b99b96a24bf84b1bd7251bb349
Jaroslav <jarrzo@outlook.com>
2021-04-23 02:56:17 +0300

    Simplify cars.controller methods / Remove deprecated function call from index.js

--- back/controllers/cars.controller.js
+++ back/controllers/cars.controller.js
@@ -28,9 +28,9 @@ const getUserCars = async(req, res) => {
     const {username} = req.params;
 
     try{
-        const user = await User.find({username}).populate("cars");
+        const cars = await Car.find({ownerUsername: username});
         
-        res.status(200).json(user[0]["cars"]);
+        res.status(200).json(cars);
     }
     catch(err){
         res.status(500).json({message: "Something wrong with server"});
@@ -41,10 +41,10 @@ const getCar = async (req, res) => {
     const {carId} = req.params;
 
     try{
-        const car = await Car.find({_id: carId});
+        const car = await Car.findById(carId);
 
-        if(car[0]){
-           return res.status(200).json(car[0]);       
+        if(car){
+           return res.status(200).json(car);       
         }
 
         res.status(400).json("Car doesn't exist");
@@ -58,15 +58,10 @@ const getUserCar = async (req, res) => {
     const {username, carId} = req.params;
 
     try{
-        const user = await User.find({username}).populate("cars");
-        const cars = user[0]["cars"];
+        const car = await Car.find({ownerUsername: username, _id: carId});
 
-        for(i = 0; i < cars.length; i++){
-            const car = cars[i];
-
-            if(car._id == carId){
-               return res.status(200).json(car);       
-            }
+        if(car){
+            return res.status(200).json(car);       
         }
 
         res.status(400).json("Car doesn't exist");
@@ -78,16 +73,7 @@ const getUserCar = async (req, res) => {
 
 const getAllCars = async (req, res) => {
     try{
-        const allUsers = await User.find().populate("cars");
-        const cars = [];
-
-        for(i = 0; i < allUsers.length; i++){
-            const currentCars = allUsers[i]["cars"];
-
-            for(j = 0; j < currentCars.length; j++){
-                cars.push(currentCars[j]);
-            }
-        }
+        const cars = Car.all();
         
         res.status(200).send(cars);   
     }
--- back/index.js
+++ back/index.js
@@ -8,7 +8,6 @@ const dbConfig = require("./config/db.config");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}))
 app.use(cors());
 app.use("/users", userRoutes);
 app.use("/cars", carRoutes);
