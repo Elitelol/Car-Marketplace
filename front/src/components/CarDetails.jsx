06e578b8c8c68069ef1beae89239f7d35c75e081
Matas <luckyrads@gmail.com>
2021-04-23 02:25:36 +0300

    FIX: Warning.

--- front/src/components/CarDetails.jsx
+++ front/src/components/CarDetails.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import CarsRepository from "../services/api/cars";
 
 export default class CarDetails extends Component {
@@ -13,7 +13,7 @@ export default class CarDetails extends Component {
   }
 
   componentDidMount() {
-    if(this.state.carId) CarsRepository.fetchCar(this);
+    if (this.state.carId) CarsRepository.fetchCar(this);
   }
 
   render() {
@@ -59,10 +59,14 @@ export default class CarDetails extends Component {
               width="500"
               height="500"
               src={this.state.data.picture}
+              alt="Car"
             />
           </div>
         </div>
-        <button className="btn btn-danger" onClick={() => CarsRepository.deleteCar(this)}>
+        <button
+          className="btn btn-danger"
+          onClick={() => CarsRepository.deleteCar(this)}
+        >
           Delete
         </button>
       </div>
